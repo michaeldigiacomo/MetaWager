93304c960068b555096d60944ddbb650c16c1bd4
Michael DiGiacomo <mdigiacomo05@me.com>
2021-11-18 11:59:40 -0500

    fixed routing, page can now launch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Discover from "./pages/Discover";
 import About from "./pages/About";
-import Search from "./pages/Search";
+// import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
+// import Routes from ""
 
 function App() {
   return (
@@ -13,10 +14,10 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
+          <Routes>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+          </Routes>
         </Wrapper>
         <Footer />
       </div>
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,13 +8,13 @@ function About() {
   return (
     <div>
       <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
-        <h1>Pupster</h1>
-        <h2>They're the Good Boys and Girls</h2>
+        <h1>MetaWager</h1>
+        <h2>Bet Away!</h2>
       </Hero>
       <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
-            <h1>Welcome To Pupster!</h1>
+            <h1>Welcome To MetaWager</h1>
           </Col>
         </Row>
         <Row>
